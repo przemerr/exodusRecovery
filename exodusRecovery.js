03868e80e325f27149d7e24fa38ddd667c3fe923
przemerr <lisk@Beast>
2024-06-10 16:29:44 +0200

    re-write

--- exodusRecovery.js
+++ exodusRecovery.js
@@ -1,58 +1,67 @@
-const { apiClient, cryptography, transactions } = require('@liskhq/lisk-client');
+const { cryptography } = require('@liskhq/lisk-client');
 const readline = require('readline');
 const nacl = require('tweetnacl');
-
-const RPC_ENDPOINT = 'wss://mainnet.lisk.io/rpc-ws';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const getClient = async () => {
-  return await apiClient.createWSClient(RPC_ENDPOINT);
-};
+rl.question('Enter your private key (hex format): ', (privateKeyHex) => {
+  rl.question('Enter your password for encryption: ', async (password) => {
+    const privateKeyBytes = Buffer.from(privateKeyHex, 'hex');
+    const publicKeyBytes = nacl.sign.keyPair.fromSeed(privateKeyBytes.slice(0, 32)).publicKey;
+    const privateKeyWithPublicKey = Buffer.concat([privateKeyBytes, publicKeyBytes]);
 
-rl.question('Enter the recipient address: ', (recipientAddress) => {
-  rl.question('Enter your private key (hex format): ', async (privateKeyHex) => {
-    rl.question('Enter the amount to transfer (in LSK): ', async (amount) => {
-      const privateKeyBytes = Buffer.from(privateKeyHex, 'hex');
-      const publicKeyBytes = nacl.sign.keyPair.fromSeed(privateKeyBytes.slice(0, 32)).publicKey;
-      const privateKeyWithPublicKey = Buffer.concat([privateKeyBytes, publicKeyBytes]);
+    try {
+      const address = cryptography.address.getAddressFromPrivateKey(privateKeyWithPublicKey);
+      const address32 = cryptography.address.getLisk32AddressFromAddress(address);
+      const publicKey = cryptography.ed.getPublicKeyFromPrivateKey(privateKeyWithPublicKey);
 
-      let client;
-      try {
-        client = await getClient();
-        const address = cryptography.address.getAddressFromPrivateKey(privateKeyBytes);
-        const address32 = cryptography.address.getLisk32AddressFromAddress(address);
-        const authData = await client.invoke('auth_getAuthAccount', { address: address32 });
-        const nonce = authData.nonce;
+      const encryptedPrivateKey = await cryptography.encrypt.encryptMessageWithPassword(
+        `{"privateKey":"${privateKeyWithPublicKey.toString('hex')}","recoveryPhrase":"dummy"}`,
+        password
+      );
 
-        const tx = await client.transaction.create({
-          module: 'token',
-          command: 'transfer',
-          nonce: BigInt(nonce),
-          fee: BigInt(transactions.convertLSKToBeddows('0.002')),
-          params: {
-            tokenID: Buffer.from('0000000000000000', 'hex'),
-            amount: BigInt(transactions.convertLSKToBeddows(amount)),
-            recipientAddress,
-            data: ''
-          }
-        }, privateKeyWithPublicKey);
+      const cryptoPart = {
+        ciphertext: encryptedPrivateKey.ciphertext,
+        mac: encryptedPrivateKey.mac,
+        kdf: 'argon2id',
+        kdfparams: {
+          parallelism: 4,
+          iterations: 1,
+          memorySize: 2097023,
+          salt: encryptedPrivateKey.kdfparams.salt,
+        },
+        cipher: 'aes-128-gcm',
+        cipherparams: {
+          iv: encryptedPrivateKey.cipherparams.iv,
+          tag: encryptedPrivateKey.cipherparams.tag,
+        },
+        version: '1'
+      };
 
-        console.log(`Transaction prepared:`, tx);
-        const res = await client.transaction.send(tx);
-        console.log('Transaction sent. Response:', res);
-      } catch (error) {
-        console.error('Error sending transaction:', error);
-      } finally {
-        if (client && client.close) {
-          
-        }
-        rl.close();
-        process.exit(0);
-      }
-    });
+      const metadataPart = {
+        name: 'exo',
+        pubkey: publicKey.toString('hex'),
+        path: "m/44'/134'/0'/0/0",
+        address: address32,
+        creationTime: new Date().toISOString(),
+      };
+
+      const result = {
+        crypto: cryptoPart,
+        metadata: metadataPart,
+        version: 1
+      };
+
+      console.log(JSON.stringify(result, null, 2));
+
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      rl.close();
+      process.exit(0);
+    }
   });
 });
